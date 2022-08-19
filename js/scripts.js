247fb4fef753325dd90bb25bb8a6157716d2e658
clapme0316 <106906197+clapme0316@users.noreply.github.com>
2022-08-19 17:43:34 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,39 @@
-$(function () {});
+// 팝업
+function openPop(num) {
+  $(".pop_bg").fadeIn();
+  $(".pop_ct" + num).fadeIn();
+  if ($(".pop_ct" + num).hasClass("fixed")) {
+    $("body").addClass("fix");
+  }
+  $(".pop_close").click(function () {
+    $(".pop_bg").fadeOut();
+    $(".pop_ct").fadeOut();
+    $("body").removeClass("fix");
+  });
+}
+
+const navtab = {
+  init: function () {
+    this.motab();
+  },
+  motab: function () {
+    let mBtn = $(".ham_btn");
+
+    // 모바일 버튼 클릭
+    $(mBtn).click(function () {
+      $(".snb").addClass("on");
+    });
+    // 화면 클릭
+    $(".content").click(function () {
+      $(".snb").removeClass("on");
+    });
+  },
+};
+
+$(document).ready(function () {
+  navtab.init();
+});
+
+$(ham_btn).click(function () {
+  $(".sub").addClass("on");
+});
